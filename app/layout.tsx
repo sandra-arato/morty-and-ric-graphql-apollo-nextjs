0d6c500b22d493ef02f1566b4bed1b63ac5cea40
Sandra Arato <sandra@idoescode.com>
2025-02-09 05:55:30 +1000

    chore: setup dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { fonts } from './fonts'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    // https://github.com/chakra-ui/chakra-ui/issues/7941#issuecomment-2614520487
+    <html lang="en" className={fonts.rubik.variable} suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
